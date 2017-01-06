17f5c204bf0eef6541421a7b6840213f4ae97ee3
t-zouchi <mojamoja.000@gmail.com>
2017-01-06 20:57:47 +0900

    update character move

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class characterController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+  Rigidbody m_Rigidbody;
+
+  // Use this for initialization
+  void Start () {
+    m_Rigidbody = GetComponent<Rigidbody>();
+  }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+    float speed = 3f;
+    float x = 0.0f;
+    float y = 0.0f;
+    float z = 0.0f;
+
+
+    if (Input.GetKey(KeyCode.D))
+    {
+      x += speed;
+    }
+    if (Input.GetKey(KeyCode.A))
+    {
+      x -= speed;
+    }
+    if (Input.GetKey(KeyCode.W))
+    {
+      z += speed;
+    }
+    if (Input.GetKey(KeyCode.S))
+    {
+      z -= speed;
+    }
+
+    m_Rigidbody.velocity = z * transform.forward + y * transform.up + x * transform.right;
+  }
 }
