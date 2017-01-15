ddbece8ea908cf920b4ea7b8433895266ca0a996
t-zouchi <mojamoja.000@gmail.com>
2017-01-15 16:41:47 +0900

    create GameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+  public GameObject enemy1;
+  public GameObject enemy2;
+  public GameObject enemy3;
+
+  // Use this for initialization
+  void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
