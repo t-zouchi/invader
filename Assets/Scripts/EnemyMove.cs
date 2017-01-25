c70f11f8b1ca2498d23723d5bb1172f008d0b6bb
t-zouchi <mojamoja.000@gmail.com>
2017-01-25 19:19:09 +0900

    Add moveForward method

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -20,10 +20,7 @@ public class EnemyMove : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
     if (moveflg != enemyMoveController.moveflg){
-      Debug.Log("forawd!");
-      z = -1.1f;
-      y = -1.1f / 2;
-      m_Rigidbody.position =(transform.right * m_Rigidbody.position.x ) + (transform.up * m_Rigidbody.position.y + new Vector3(0, y, 0)) + (transform.forward *m_Rigidbody.position.z + new Vector3(0, 0, z));
+      moveForward();
     }
 
       moveflg = enemyMoveController.moveflg;
@@ -43,5 +40,13 @@ public class EnemyMove : MonoBehaviour {
   {
     enemyMoveController.moveflg = moveflg * -1;
     moveflg = enemyMoveController.moveflg;
+    moveForward();
+  }
+
+  void moveForward()
+  {
+    z = -1.1f;
+    y = -1.1f / 2;
+    m_Rigidbody.position = (transform.right * m_Rigidbody.position.x) + (transform.up * m_Rigidbody.position.y + new Vector3(0, y, 0)) + (transform.forward * m_Rigidbody.position.z + new Vector3(0, 0, z));
   }
 }
