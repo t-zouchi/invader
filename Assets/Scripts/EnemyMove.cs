5f2e45906274b649ab4fd418a2c98db95733122e
t-zouchi <mojamoja.000@gmail.com>
2017-01-25 19:15:41 +0900

    Add move foward logic

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -7,6 +7,8 @@ public class EnemyMove : MonoBehaviour {
   Rigidbody m_Rigidbody;
   int moveflg = 1;
   float x = 0;
+  float y = 0;
+  float z = 0;
   Vector3 move;
  
   // Use this for initialization
@@ -17,7 +19,14 @@ public class EnemyMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-    moveflg = enemyMoveController.moveflg;
+    if (moveflg != enemyMoveController.moveflg){
+      Debug.Log("forawd!");
+      z = -1.1f;
+      y = -1.1f / 2;
+      m_Rigidbody.position =(transform.right * m_Rigidbody.position.x ) + (transform.up * m_Rigidbody.position.y + new Vector3(0, y, 0)) + (transform.forward *m_Rigidbody.position.z + new Vector3(0, 0, z));
+    }
+
+      moveflg = enemyMoveController.moveflg;
 		if(moveflg == 1)
     {
       x = 1;
