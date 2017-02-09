95a5c18c01ce44c5842f1fa694e5819f983b830b
t-zouchi <mojamoja.000@gmail.com>
2017-02-09 21:32:45 +0900

    clean code

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -15,36 +15,12 @@ public class characterController : MonoBehaviour {
 	
 	void Update () {
     Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
-    float speed = 3f;
-    float inputHorizontal = 0.0f; //横
-    float inputVertical = 0.0f;  //前後
-
-/*    if (Input.GetKey(KeyCode.D))
-    {
-      inputHorizontal = 1f;
-    }
-    else if (Input.GetKey(KeyCode.A))
-    {
-      inputHorizontal = -1f;
-    }
-
-    if (Input.GetKey(KeyCode.W))
-    {
-      inputVertical = 1f;
-    }
-    else if (Input.GetKey(KeyCode.S))
-    {
-      inputVertical = -1f;
-    }*/
-
     if (Input.GetMouseButtonDown(0))
     {
       Debug.Log("ばぁん");
       shot();
     }
 
-    //Vector3 moveForward = cameraForward * inputVertical + Camera.main.transform.right * inputHorizontal;
-    //m_Rigidbody.velocity = moveForward * speed;
   }
 
   void shot()
